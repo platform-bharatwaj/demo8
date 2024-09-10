d8016651c4781e99bb9e803f3439859a6b7ec7c2
bharat <bharat@192.168.1.222>
2024-09-10 11:30:46 +0530

    Initial commit

---
+++ src/main/java/com/intuit/demo8/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.intuit.demo8;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "You have successfully created Java Webservice DEMO8 using wizard. Awesome right!!!";
+    }
+
+}
+
